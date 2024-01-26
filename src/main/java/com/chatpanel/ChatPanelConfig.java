e74ef7074542d4e957b0e57c878d85cb8834e7a3
Yenof <Yenofniapo+git@gmail.com>
2024-01-26 16:14:56 -0700

    Add tab selection, configs for new tabs, pop out size
    Added ability to turn tabs on or off
    Added corresponding configs for new tabs
    Added section for pop out window settings moved related things there
    Added option to set the initial size of the pop out window.

--- src/main/java/com/chatpanel/ChatPanelConfig.java
+++ src/main/java/com/chatpanel/ChatPanelConfig.java
@@ -31,6 +31,56 @@ public interface ChatPanelConfig extends Config {
     )
     String generalSection = "general";
 
+    @ConfigSection(
+            name = "Public Chat",
+            description = "Settings for public chat",
+            position = 2
+    )
+    String publicChatSection = "publicChat";
+
+    @ConfigSection(
+            name = "Private Chat",
+            description = "Settings for private chat",
+            position = 3
+    )
+    String privateChatSection = "privateChat";
+
+    @ConfigSection(
+            name = "Clan Chat",
+            description = "Settings for clan chat",
+            position = 4
+    )
+    String clanChatSection = "clanChat";
+    @ConfigSection(
+            name = "Friends Chat",
+            description = "Settings for friends chat",
+            position = 4
+    )
+    String friendsChatSection = "friendsChat";
+    @ConfigSection(
+            name = "Game Chat",
+            description = "Settings for game chat",
+            position = 6
+    )
+    String gameChatSection = "gameChat";
+    @ConfigSection(
+            name = "All Chat",
+            description = "Settings for the All Chat tab, not all of the chat.",
+            position = 7
+    )
+    String allChatSection = "allChat";
+    @ConfigSection(
+            name = "Pop out Window",
+            description = "Settings for the pop out window",
+            position = 1
+    )
+    String popoutSection = "popoutwindow";
+    @ConfigSection(
+            name = "Tabs",
+            description = "Tab selection. (Recommended 4. Tab changes require plugin restart.)",
+            position = 7
+    )
+    String tabSection = "tabselection";
     @ConfigItem(
             keyName = "fontStyle",
             name = "Font Style",
@@ -61,34 +111,6 @@ public interface ChatPanelConfig extends Config {
     default boolean use24HourFormat() {
         return false;
     }
-    @ConfigSection(
-            name = "Public Chat",
-            description = "Settings for public chat",
-            position = 1
-    )
-    String publicChatSection = "publicChat";
-
-    @ConfigSection(
-            name = "Private Chat",
-            description = "Settings for private chat",
-            position = 2
-    )
-    String privateChatSection = "privateChat";
-
-    @ConfigSection(
-            name = "Clan Chat",
-            description = "Settings for clan chat",
-            position = 3
-    )
-    String clanChatSection = "clanChat";
-
-    @ConfigSection(
-            name = "Game Chat",
-            description = "Settings for game chat",
-            position = 4
-    )
-    String gameChatSection = "gameChat";
-
     @Range(min = 50, max = 5000)
     @ConfigItem(
             keyName = "chatAreaHeight",
@@ -198,6 +220,38 @@ public interface ChatPanelConfig extends Config {
     {
         return 12;
     }
+    @ConfigItem(
+            keyName = "friendsChatColor",
+            name = "Friends Chat Color",
+            description = "Configures the font color of the Friends Chat messages.",
+            section = friendsChatSection
+    )
+    default Color friendsChatColor()
+    {
+        return new Color(0x28FF71);
+    }
+
+    @ConfigItem(
+            keyName = "friendsChatBackground",
+            name = "Friends Chat Background color",
+            description = "Configures the background color of the Friends Chat.",
+            section = friendsChatSection
+    )
+    default Color friendsChatBackground()
+    {
+        return new Color(0x282828);
+    }
+    @Range(min = 5, max = 50)
+    @ConfigItem(
+            keyName = "friendsChatFontSize",
+            name = "Friends Chat Font Size",
+            description = "Configures the font size of the Friends Chat messages. (Max 50)",
+            section = friendsChatSection
+    )
+    default int friendsChatFontSize()
+    {
+        return 10;
+    }
 
     @ConfigItem(
             keyName = "gameChatColor",
@@ -231,6 +285,38 @@ public interface ChatPanelConfig extends Config {
     {
         return 12;
     }
+    @ConfigItem(
+            keyName = "allChatColor",
+            name = "All Chat Color",
+            description = "Configures the font color of the All Chat messages.",
+            section = allChatSection
+    )
+    default Color allChatColor()
+    {
+        return new Color(0xFFFFFF);
+    }
+
+    @ConfigItem(
+            keyName = "allChatBackground",
+            name = "All Chat Background color",
+            description = "Configures the background color of the All Chat.",
+            section = allChatSection
+    )
+    default Color allChatBackground()
+    {
+        return new Color(0x282828);
+    }
+    @Range(min = 5, max = 50)
+    @ConfigItem(
+            keyName = "allChatFontSize",
+            name = "All Chat Font Size",
+            description = "Configures the font size of the All Chat messages. (Max 50)",
+            section = allChatSection
+    )
+    default int allChatFontSize()
+    {
+        return 10;
+    }
 
     @Range ( max =10)
     @ConfigItem(
@@ -247,10 +333,71 @@ public interface ChatPanelConfig extends Config {
             keyName = "popoutAlwaysOnTop",
             name = "Pop out Always on Top",
             description = "Keep the pop out window always on top of other windows",
-            section = generalSection,
-            position = 7
+            section = popoutSection,
+            position = 1
     )
     default boolean popoutAlwaysOnTop() {
         return false;
     }
+    @ConfigItem(
+            keyName = "popoutSize",
+            name = "Size",
+            description = "Set the initial size of the pop out window",
+            section = popoutSection,
+            position = 0
+    )
+    default Dimension popoutSize()
+    {
+        return new Dimension(300, 400);
+    }
+    @ConfigItem(
+            keyName = "showPublicChat",
+            name = "Show Public Chat",
+            description = "Show/hide the Public Chat tab (Requires plugin restart)",
+            section = tabSection,
+            position = 0
+    )
+    default boolean showPublicChat() {return true;}
+    @ConfigItem(
+            keyName = "showPrivateChat",
+            name = "Show Private Chat",
+            description = "Show/hide the Private Chat tab (Requires plugin restart)",
+            section = tabSection,
+            position = 1
+    )
+    default boolean showPrivateChat() {return true;}
+    @ConfigItem(
+            keyName = "showClanChat",
+            name = "Show Clan Chat",
+            description = "Show/hide the Clan Chat tab (Requires plugin restart)",
+            section = tabSection,
+            position = 2
+    )
+    default boolean showClanChat() {return true;}
+    @ConfigItem(
+            keyName = "showGameChat",
+            name = "Show Game Chat",
+            description = "Show/hide the Game tab (Requires plugin restart)",
+            section = tabSection,
+            position = 4
+    )
+    default boolean showGameChat() {return true;}
+    @ConfigItem(
+            keyName = "showAllChat",
+            name = "Show All Chat",
+            description = "Show/hide the All Chat tab (Requires plugin restart)",
+            section = tabSection,
+            position = 5
+    )
+    default boolean showAllChat() {
+        return false;
+    }
+    @ConfigItem(
+            keyName = "showFriendsChat",
+            name = "Show Friends Chat Channel",
+            description = "Show/hide the Friends Chat tab (Requires plugin restart)",
+            section = tabSection,
+            position = 3
+    )
+    default boolean showFriendsChat() {return false;}
 }
